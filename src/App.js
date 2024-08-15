79f18489fdaa00efc757f441445427d3e0b6fab7
Leslie <Kooliesk@gmail.com>
2024-08-15 22:46:19 +0200

    create projects and contact pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './routes/Home'
+import Projects from './routes/Projects'
+import Services from './routes/Services'
+import Contact from './routes/Contact'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer'
 
@@ -10,6 +13,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/services' element={<Services/>}></Route>
+        <Route path='/projects' element={<Projects/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
       </Routes>
       <Footer/>
     </BrowserRouter>
---
+++ src/routes/Services.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ServicesPage from "../components/servicesPage";
+
+const Services = () => {
+    return (
+        <ServicesPage />
+    )
+}
+
+export default Services;
